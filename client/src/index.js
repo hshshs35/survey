60861f98a540ed244b1a36cdafb79d00de63728b
Huang Sha <huangsha@172-17-87-200.DYNAPOOL.NYU.EDU>
2017-12-17 21:50:54 -0500

    api setting

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import{ createStore, applyMiddleware } from 'redux';
+import 'materialize-css/dist/css/materialize.css';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
+import reducers from './reducers';
 
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
-ReactDOM.render(<App/>,
-    document.querySelector('#root'));
+ReactDOM.render(
+    <Provider store={store}><App/></Provider>,
+    document.querySelector('#root')
+);
+
+console.log('STRIPE_KEY is ', process.env.REACT_APP_STRIPE_KEY);
+console.log('Environment is ', process.env.NODE_ENV);
