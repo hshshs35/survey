189a9a58ac055fe608a1f0229f9ff5dd6e4d500e
Huang Sha <huangsha@172-17-82-64.DYNAPOOL.NYU.EDU>
2017-12-21 19:30:38 -0500

    homepage work

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
+
 const Landing = () => {
     return (
-        <div style={{textAlign: 'center'}}>
-            <h1>
-                Smart Feedback Service
-            </h1>
-            Collect feedback from your users
+
+        <div>
+            <div>
+                <div class="row">
+                    <div class="col s12 m12">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src={ require('./images/simple1.jpg') } />
+                                <span class="card-title">Smart Feedback Service</span>
+                            </div>
+                            <div class="card-content">
+                                <p>Welcome to my world</p>
+                            </div>
+                            <div class="card-action">
+                                <a href="/auth/google">Login with Google to start</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
-}
+};
 
 export default Landing;
