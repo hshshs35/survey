8c1b69b852d605a2d38bcd5de464da1a3c858fc8
Huang Sha <huangsha@172-17-82-64.DYNAPOOL.NYU.EDU>
2017-12-21 19:42:38 -0500

    add the sample survey

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,6 +5,18 @@ import SurveyList from './surveys/SurveyList';
 const DashBoard = () => {
     return (
         <div>
+            <div className="card lighten-3" key="1">
+                <div className="card-content">
+                    <span className="card-title">Sample card survey</span>
+                    <p>
+                        Press the red button at the bottom right corner to send a new survey
+                    </p>
+                </div>
+                <div className="card-action">
+                    <a>Please make sure you have at least one credit to start</a>
+                </div>
+            </div>
+
             <SurveyList/>
             <div className="fixed-action-btn">
                 <Link to="/surveys/new" className="btn-floating btn-large red">
